6bf303d0b1c55d0923f1a2d7956264104dd7c47a
apaul@transparent.com <apaul@transparent.com>
2017-02-22 11:14:41 -0500

    Multiprocessing update with test discovery

--- atest/__main__.py
+++ atest/__main__.py
@@ -1,12 +1,7 @@
-import unittest
-import os
+import nose
+from constants import *
 
 if __name__ == "__main__":
-    try:
-        import xmlrunner
-    except ImportError:
-        unittest.main()
-    else:
-        if not os.path.exists("test-reports"):
-            os.makedirs("test-reports")
-        unittest.main(verbosity=2, testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
+    if not os.path.exists("test-reports"):
+        os.makedirs("test-reports")
+    nose.main()
