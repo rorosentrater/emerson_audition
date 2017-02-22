7e35e0c366bdb43b8a94dc3cb69be9002e6e0759
apaul@transparent.com <apaul@transparent.com>
2017-02-22 11:37:04 -0500

    Client discovery (stuff like that, whatever, should be fine)

--- library/setup.template.py
+++ library/setup.template.py
@@ -1,12 +1,12 @@
 __version__ = "0.0.1"
 NAME = "transparent_qat-template"
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name=NAME,
     version=__version__,
-    packages=["transparent_qat"],
+    packages=find_packages(),
     namespace_packages=["transparent_qat"],
     install_requires=[
         "selenium==3.0.0b3",
