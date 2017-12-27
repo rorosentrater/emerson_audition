35acf44069acd06e0cfe949a53108747ce881039
John Nolette <john@neetgroup.net>
2017-12-27 10:57:51 -0500

    Added test_module support

--- atest/constants.py
+++ atest/constants.py
@@ -1,10 +1,10 @@
 import os
 
 
+TEST_MODULE = os.environ.get('TEST_MODULE', '')
 COMMAND_EXECUTOR = os.environ.get('COMMAND_EXECUTOR', '')
 
 os.environ.setdefault('NOSE_PROCESSES', '10')
 os.environ.setdefault('NOSE_PROCESS_TIMEOUT', '18000')
-
 os.environ.setdefault('NOSE_WITH_XUNITMP', 'TRUE')
 os.environ.setdefault('NOSE_XUNITMP_FILE', 'test-reports/xunit.xml')
