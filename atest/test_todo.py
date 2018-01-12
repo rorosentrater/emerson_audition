190ad9233c18c799b3464b4dc2a8ef395c19ac02
Logan Kurr <lkurr@transparent.com>
2018-01-12 10:49:16 -0500

    - No longer using Controller

--- atest/test_todo.py
+++ atest/test_todo.py
@@ -1,25 +1,26 @@
 from unittest import TestCase
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from library.qat.todo import App
 
 
 class VerifyRender(TestCase):
 
     def test_page_render(self):
         """Calls test_rendered on necessary elements to verify they render as expected"""
-        app = App(webdriver.Firefox(), 'https://riot-todo-84334.firebaseapp.com/#!/', {})
+        self.driver = webdriver.Firefox()
+        self.driver.get("https://riot-todo-84334.firebaseapp.com/#!/")
         try:
-            assert App.test_rendered(app, By.ID, "deleteTasks")
+            assert self.driver.find_element(By.ID, "deleteTasks")
             # Verifies Delete Completed Button renders correctly
-            assert App.test_rendered(app, By.CLASS_NAME, "is-success.u-pull-right")
+            assert self.driver.find_element(By.CLASS_NAME, "is-success.u-pull-right")
             # Verifies Create Task Button renders correctly
-            assert App.test_rendered(app, By.ID, "social")
+            assert self.driver.find_element(By.ID, "social")
             # Verifies list of Social widgets renders correctly
-            assert App.test_rendered(app, By.CLASS_NAME, "logo.animated.slideInDown")
+            assert self.driver.find_element(By.CLASS_NAME, "logo.animated.slideInDown")
             # Verifies To-Do header renders correctly
-            assert App.test_rendered(app, By.CLASS_NAME, "unstyled")
+            assert self.driver.find_element(By.CLASS_NAME, "unstyled")
             # Verifies list of Tasks renders correctly
         finally:
-            # Terminates Webdriver"""
-            app.browser.quit()
+            self.driver.quit()
+            # Terminates Webdriver
--- library/qat/todo/todo.py
+++ library/qat/todo/todo.py
@@ -1,15 +1,7 @@
 from pyscc import Controller
-from selenium.common.exceptions import NoSuchElementException
 
 
 class App(Controller):
 
-    def test_rendered(self, how, what):
-        """Method to test rendering of elements"""
-        try:
-            # Takes parameters to locate an element in the current browser
-            self.browser.find_element(by=how, value=what)
-        except NoSuchElementException:
-            # Throws exception if element is not found
-            return False
-        return True
+    def foobar(self):
+        return 'hello world'
