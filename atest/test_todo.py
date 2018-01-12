0239a4db63884bdbea2610952c53b2e15d31af70
Logan Kurr <lkurr@transparent,com>
2018-01-12 13:01:44 -0500

    - Removed try/except

--- atest/test_todo.py
+++ atest/test_todo.py
@@ -8,23 +8,20 @@ class VerifyRender(TestCase):
 
     def test_page_render(self):
         """Calls test_rendered on necessary elements to verify they render as expected"""
-        self.driver = webdriver.Firefox()
-        self.driver.get("https://riot-todo-84334.firebaseapp.com/#!/")
-        try:
-            assert self.driver.find_element(By.ID, "deleteTasks")
-            # Verifies Delete Completed Button renders correctly
-            assert self.driver.find_element(By.CLASS_NAME, "is-success.u-pull-right")
-            # Verifies Create Task Button renders correctly
-            assert self.driver.find_element(By.ID, "social")
-            # Verifies list of Social widgets renders correctly
-            assert self.driver.find_element(By.CLASS_NAME, "logo.animated.slideInDown")
-            # Verifies To-Do header renders correctly
-            assert self.driver.find_element(By.CLASS_NAME, "unstyled")
-            # Verifies list of Tasks renders correctly
-        finally:
-            self.driver.quit()
-            # Terminates Webdriver
-
+        driver = webdriver.Firefox()
+        driver.get("https://riot-todo-84334.firebaseapp.com/#!/")
+        assert driver.find_element(By.ID, "deleteTasks")
+        # Verifies Delete Completed Button renders correctly
+        assert driver.find_element(By.CLASS_NAME, "is-success.u-pull-right")
+        # Verifies Create Task Button renders correctly
+        assert driver.find_element(By.ID, "social")
+        # Verifies list of Social widgets renders correctly
+        assert driver.find_element(By.CLASS_NAME, "logo.animated.slideInDown")
+        # Verifies To-Do header renders correctly
+        assert driver.find_element(By.CLASS_NAME, "unstyled")
+        # Verifies list of Tasks renders correctly
+        driver.quit()
+        # Terminates Webdriver
 
 
     def test_todo_bl(self):
