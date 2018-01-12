a7f62851cc079e00168361a92179d564f3f40f75
blougee <blougee@transparent.com>
2018-01-12 11:57:23 -0500

    exercise 1

--- atest/test_todo.py
+++ atest/test_todo.py
@@ -4,19 +4,21 @@ from selenium import webdriver
 
 class TestTodo(TestCase):
 
-    # foobar
 
-    driver = webdriver.Firefox()
-    driver.get("https://riot-todo-84334.firebaseapp.com/#!/")
 
-    webtitle = driver.find_element_by_id("task-1")
-    tsk2 = driver.find_element_by_id("task-2")
-    tsk3 = driver.find_element_by_id("task-3")
-    deletebutton = driver.find_element_by_class_name("is-danger.u-pull-left")
-    createbutton = driver.find_element_by_class_name("is-success.u-pull-right")
 
-    twitt = driver.find_element_by_class_name("ico.ico-mat.fi-social-twitter")
-    linked = driver.find_element_by_class_name("ico.ico-mat.fi-social-linkedin")
-    git = driver.find_element_by_class_name("ico.ico-mat.fi-social-github")
+    def test_toDo(self):
+        driver = webdriver.Firefox()
+        driver.get("https://riot-todo-84334.firebaseapp.com/#!/")
 
-    driver.close()
+        assert driver.find_element_by_id("task-1")
+        assert driver.find_element_by_id("task-2")
+        assert driver.find_element_by_id("task-3")
+        assert driver.find_element_by_class_name("is-danger.u-pull-left")
+        assert driver.find_element_by_class_name("is-success.u-pull-right")
+
+        assert driver.find_element_by_class_name("ico.ico-mat.fi-social-twitter")
+        assert driver.find_element_by_class_name("ico.ico-mat.fi-social-linkedin")
+        assert driver.find_element_by_class_name("ico.ico-mat.fi-social-github")
+
+        driver.close()
