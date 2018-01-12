cdece212db0f4c7a4aa09563332f4db1e0e4efe4
blougee <blougee@transparent.com>
2018-01-12 10:58:51 -0500

    exercise 1

--- atest/test_todo.py
+++ atest/test_todo.py
@@ -1,8 +1,20 @@
 from unittest import TestCase
+from selenium import webdriver
 
 
 class TestTodo(TestCase):
 
-    def test_foobar(self):
-        """this test tests foobar"""
-        assert True
+    driver = webdriver.Firefox()
+    driver.get("https://riot-todo-84334.firebaseapp.com/#!/")
+
+    webtitle = driver.find_element_by_id("task-1")
+    tsk2 = driver.find_element_by_id("task-2")
+    tsk3 = driver.find_element_by_id("task-3")
+    deletebutton = driver.find_element_by_class_name("is-danger.u-pull-left")
+    createbutton = driver.find_element_by_class_name("is-success.u-pull-right")
+
+    twitt = driver.find_element_by_class_name("ico.ico-mat.fi-social-twitter")
+    linked = driver.find_element_by_class_name("ico.ico-mat.fi-social-linkedin")
+    git = driver.find_element_by_class_name("ico.ico-mat.fi-social-github")
+
+    driver.close()
