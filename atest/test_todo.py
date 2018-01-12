f68b46aac78b73e3d1a41c97aaf98197cae7bcd9
blougee <blougee@transparent.com>
2018-01-12 12:13:41 -0500

    exercise 1

--- atest/test_todo.py
+++ atest/test_todo.py
@@ -7,18 +7,18 @@ class TestTodo(TestCase):
 
 
 
-    def test_toDo(self):
+    def test_todo_bl(self):
         driver = webdriver.Firefox()
         driver.get("https://riot-todo-84334.firebaseapp.com/#!/")
 
-        assert driver.find_element_by_id("task-1")
-        assert driver.find_element_by_id("task-2")
-        assert driver.find_element_by_id("task-3")
-        assert driver.find_element_by_class_name("is-danger.u-pull-left")
-        assert driver.find_element_by_class_name("is-success.u-pull-right")
+        driver.find_element_by_id("task-1")
+        driver.find_element_by_id("task-2")
+        driver.find_element_by_id("task-3")
+        driver.find_element_by_class_name("is-danger.u-pull-left")
+        driver.find_element_by_class_name("is-success.u-pull-right")
 
-        assert driver.find_element_by_class_name("ico.ico-mat.fi-social-twitter")
-        assert driver.find_element_by_class_name("ico.ico-mat.fi-social-linkedin")
-        assert driver.find_element_by_class_name("ico.ico-mat.fi-social-github")
+        driver.find_element_by_class_name("ico.ico-mat.fi-social-twitter")
+        driver.find_element_by_class_name("ico.ico-mat.fi-social-linkedin")
+        driver.find_element_by_class_name("ico.ico-mat.fi-social-github")
 
         driver.close()
