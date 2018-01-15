f6d649a385126b2bb12ea18f9febdc2fceeb57fc
blougee <blougee@transparent.com>
2018-01-15 17:49:15 -0500

    exercise 2 a

--- library/qat/todo/components/page.py
+++ library/qat/todo/components/page.py
@@ -8,3 +8,19 @@ class Page(Component):
     @component_element
     def button(self):
         return 'button'
+
+
+    def task_assignee(self):
+        return self.browser.find_element_by_id("taskAssignee")
+
+    def task_title(self):
+        return self.browser.find_element_by_id("taskTitle")
+
+    def task_content(self):
+        return self.browser.find_element_by_id("taskContent")
+
+    def form_submit(self):
+        return self.browser.find_element_by_class_name("is-success.u-pull-right")
+
+    def task(self, index):
+        return self.browser.find_element_by_css_selector("li:nth-of-type(" + index + ")")
--- library/qat/todo/todo.py
+++ library/qat/todo/todo.py
@@ -4,5 +4,19 @@ from .components import Page
 
 class App(Controller):
 
-    def foobar(self):
-        return 'hello world'
+    def __init__(self, browser, url):
+
+        super(App, self).__init__(browser, url, {})
+        self.my_page = Page(self)
+
+    def form_fill_out(self, a, b, c):
+
+        self.my_page.task_assignee().send_keys(a)
+        self.my_page.task_title().send_keys(b)
+        self.my_page.task_content().send_keys(c)
+        self.my_page.form_submit().click()
+
+    def task_created(self, index):
+        self.my_page.task(self, index)
+
+
