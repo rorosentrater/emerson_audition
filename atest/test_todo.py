0a02d8f6b3dfcbbc9b79a55849d93156ae1c7fed
Logan Kurr <lkurr@transparent,com>
2018-01-16 10:58:59 -0500

    - Task Creation Exercise

--- atest/test_todo.py
+++ atest/test_todo.py
@@ -1,8 +1,27 @@
 from unittest import TestCase
+from selenium import webdriver
+
+from selenium.webdriver.common.by import By
 
 
-class TestTodo(TestCase):
+class CreateTask(TestCase):
 
-    def test_foobar(self):
-        """this test tests foobar"""
-        assert True
+    def setUp(self):
+        """Creates Firefox Webdriver instance"""
+        self.driver = webdriver.Firefox()
+        self.driver.get('https://riot-todo-84334.firebaseapp.com/#!/')
+
+    def test_create_task(self):
+        """Creates a new task"""
+        self.driver.find_element(By.ID, 'taskAssignee').send_keys('Logan')
+        # Fills out the Assignee field with some text
+        self.driver.find_element(By.ID, 'taskTitle').send_keys('Shopping List')
+        # Fills out the Title field with some text
+        self.driver.find_element(By.ID, 'taskContent').send_keys('Milk, Eggs, Cheese')
+        # Fills out the Content field with some text
+        self.driver.find_element(By.CLASS_NAME, 'is-success.u-pull-right').click()
+        # Clicks Create Button to Create the task
+
+    def tearDown(self):
+        """Closes Browser"""
+        self.driver.quit()
