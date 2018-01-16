958bc3cdab17e102719fa38230679de7b830fffb
blougee <blougee@transparent.com>
2018-01-15 19:07:21 -0500

    exercise 2 a

--- library/qat/todo/components/page.py
+++ library/qat/todo/components/page.py
@@ -9,17 +9,22 @@ class Page(Component):
     def button(self):
         return 'button'
 
+    @property
     def task_assignee(self):
         return self.browser.find_element_by_id("taskAssignee")
 
+    @property
     def task_title(self):
         return self.browser.find_element_by_id("taskTitle")
 
+    @property
     def task_content(self):
         return self.browser.find_element_by_id("taskContent")
 
+    @property
     def form_submit(self):
         return self.browser.find_element_by_class_name("is-success.u-pull-right")
+
 
     def task(self, index):
         param = "li:nth-of-type(" + str(index) + ")"
--- library/qat/todo/todo.py
+++ library/qat/todo/todo.py
@@ -11,10 +11,10 @@ class App(Controller):
 
     def form_fill_out(self, a, b, c):
 
-        self.my_page.task_assignee().send_keys(a)
-        self.my_page.task_title().send_keys(b)
-        self.my_page.task_content().send_keys(c)
-        self.my_page.form_submit().click()
+        self.my_page.task_assignee.send_keys(a)
+        self.my_page.task_title.send_keys(b)
+        self.my_page.task_content.send_keys(c)
+        self.my_page.form_submit.click()
 
     def task_created(self, index):
         self.my_page.task(index)
