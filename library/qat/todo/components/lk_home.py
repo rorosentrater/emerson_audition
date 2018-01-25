59a0743fea7510c8eb8dff2aeabda67b5cafea93
Logan Kurr <lkurr@transparent,com>
2018-01-25 13:05:54 -0500

    Added header module

--- library/qat/todo/components/lk_home.py
+++ library/qat/todo/components/lk_home.py
@@ -4,10 +4,6 @@ from pyscc import Component, component_element,  component_group
 class Home(Component):
 
     @component_element
-    def logo(self):
-        return 'a h1.logo.animated.slideInDown'
-
-    @component_element
     def task_header(self):
         return '.seven.columns h1.logo'
 
@@ -16,29 +12,21 @@ class Home(Component):
         return '.five.columns h1.logo'
 
     @component_group
-    def social(self):
-        return {
-            'social': '#social',
-            'twitter': 'a i.ico.ico-mat.fi-social-twitter',
-            'linkedin': 'a i.ico.ico-mat.fi-social-linkedin',
-            'github': 'a i.ico.ico-mat.fi-social-github'
-        }
-
-    @component_group
     def task(self):
         return {
+            '_': 'todo-list',
             'task': 'todo-task',
             'checkbox': 'input[type="checkbox"]',
-            'created_content': 'todo-task > h4',
-            'date': 'todo-task i.ico.ico-left.fi-calendar',
-            'assignee': 'todo-task #assignee',
+            'created_content': 'h4',
+            'date': 'i.ico.ico-left.fi-calendar',
+            'assignee': '#assignee',
             'delete': '#deleteTasks'
         }
 
     @component_group
     def create(self):
         return {
-            'form': 'create-todo > form',
+            '_': 'create-todo',
             'assignee': '#taskAssignee',
             'title': '#taskTitle',
             'content': '#taskContent',
--- library/qat/todo/components/lk_profile.py
+++ library/qat/todo/components/lk_profile.py
@@ -4,25 +4,25 @@ from pyscc import Component, component_element,  component_group
 class Profile(Component):
 
     @component_element
-    def logo(self):
-        return 'a h1.logo.animated.slideInDown'
-
-    @component_group
-    def social(self):
-        return {
-            'social': '#social',
-            'twitter': 'a i.ico.ico-mat.fi-social-twitter',
-            'linkedin': 'a i.ico.ico-mat.fi-social-linkedin',
-            'github': 'a i.ico.ico-mat.fi-social-github'
-        }
-
-    @component_group
     def profile(self):
-        return {
-            'profile': 'r-view',
-            'name': 'r-view > profile > h1',
-            'latest_task': 'r-view > profile > h4',
-            'task_link': 'r-view > profile > h4 > a',
-            'stat_header': 'r-view > profile > h2',
-            'stats': '#stats'
-        }
+        return 'r-view'
+
+    @component_element
+    def name(self):
+        return 'r-view > profile > h1'
+
+    @component_element
+    def latest_task(self):
+        return 'r-view > profile > h4'
+
+    @component_element
+    def task_link(self):
+        return 'r-view > profile > h4 > a'
+
+    @component_element
+    def stat_header(self):
+        return 'r-view > profile > h4 > a'
+
+    @component_element
+    def stats(self):
+        return '#stats'
