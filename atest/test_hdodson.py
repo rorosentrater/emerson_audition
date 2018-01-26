681660c41008ce7cfc0b024ca394ca482a94d33d
Harry Dodson <hdodson@transparent.com>
2018-01-26 10:14:12 -0500

    Removed exercise 1. Completed the first part of exercise 2, create a task and verify rendering.

--- atest/test_hdodson.py
+++ atest/test_hdodson.py
@@ -2,25 +2,26 @@ from selenium import webdriver
 from unittest import TestCase
 
 
-class HomepageRendering(TestCase):
+# Exercise 2
+class TaskCreation(TestCase):
 
-    def test_element_rendering(self):
-        driver = webdriver.Firefox()
-        driver.get("https://riot-todo-84334.firebaseapp.com/#!/")
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.get("https://riot-todo-84334.firebaseapp.com/#!/")
 
-        # Confirm To-Do render
-        self.assertTrue(driver.find_element_by_css_selector(".logo.animated.slideInDown"))
-        # Confirm Social Media links render
-        self.assertTrue(driver.find_element_by_css_selector(".fi-social-twitter"))
-        self.assertTrue(driver.find_element_by_css_selector(".fi-social-linkedin"))
-        self.assertTrue(driver.find_element_by_css_selector(".fi-social-github"))
-        # Confirm tasks render
-        self.assertTrue(driver.find_element_by_css_selector("#task-1"))
-        self.assertTrue(driver.find_element_by_css_selector("#task-2"))
-        self.assertTrue(driver.find_element_by_css_selector("#task-3"))
-        # Confirm create task button renders
-        self.assertTrue(driver.find_element_by_css_selector(".is-success.u-pull-right"))
-        # Confirm deleted completed task button renders
-        self.assertTrue(driver.find_element_by_css_selector(".is-danger.u-pull-left"))
+    def test_creation(self):
+        # Gets the number of current tasks
+        count = len(self.driver.find_elements_by_css_selector("li.animated.fadeIn"))
 
-        driver.quit()
+        # Create a new task
+        self.driver.find_element_by_css_selector("#taskAssignee").send_keys("Lee")
+        self.driver.find_element_by_css_selector("#taskTitle").send_keys("Nothing to do here")
+        self.driver.find_element_by_css_selector("#taskContent").send_keys("Seriously")
+        self.driver.find_element_by_css_selector(".is-success.u-pull-right").click()
+
+        # Confirms a new task was created
+        self.assertTrue(self.driver.find_element_by_css_selector("li.animated.fadeIn:nth-child({})".format(count+1)))
+
+    def tearDown(self):
+        self.driver.quit()
+
