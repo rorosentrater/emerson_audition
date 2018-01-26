c8a2340249e6cce1e5c14d5705c595047e694381
Harry Dodson <hdodson@transparent.com>
2018-01-26 14:59:23 -0500

    Added parameters for text fields when creating a task.

--- atest/test_hdodson.py
+++ atest/test_hdodson.py
@@ -13,13 +13,13 @@ class TaskCreation(TestCase):
         # Gets the number of current tasks
         count = len(self.driver.find_elements_by_css_selector("li.animated.fadeIn"))
         # Create a new task
-        create_default_task(self.driver)
+        create_default_task(self.driver, "Lee", "Nothing to do here", "Seriously")
         # Confirms a new task was created
         self.assertTrue(self.driver.find_element_by_css_selector("li.animated.fadeIn:nth-child({})".format(count + 1)))
 
     def test_assignee_link(self):
         # Create a new task
-        create_default_task(self.driver)
+        create_default_task(self.driver, "Lee", "Nothing to do here", "Seriously")
         # Click assignee profile
         assignee_elements = self.driver.find_elements_by_css_selector("span#assignee")
         assignee_elements[-1].click()
@@ -30,8 +30,8 @@ class TaskCreation(TestCase):
         self.driver.quit()
 
 
-def create_default_task(web_driver):
-    web_driver.find_element_by_css_selector("#taskAssignee").send_keys("Lee")
-    web_driver.find_element_by_css_selector("#taskTitle").send_keys("Nothing to do here")
-    web_driver.find_element_by_css_selector("#taskContent").send_keys("Seriously")
+def create_default_task(web_driver, assignee, title, content):
+    web_driver.find_element_by_css_selector("#taskAssignee").send_keys(assignee)
+    web_driver.find_element_by_css_selector("#taskTitle").send_keys(title)
+    web_driver.find_element_by_css_selector("#taskContent").send_keys(content)
     web_driver.find_element_by_css_selector(".is-success.u-pull-right").click()
