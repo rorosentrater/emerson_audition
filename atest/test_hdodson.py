0023297f3bb23c89116eb518149d5ac125733f4c
Harry Dodson <hdodson@transparent.com>
2018-01-29 13:33:39 -0500

    Completed exercise 3.

--- atest/test_hdodson.py
+++ atest/test_hdodson.py
@@ -1,33 +1,32 @@
-from selenium import webdriver
 from unittest import TestCase
+from qat.utils import Decorators
 
 
-# Exercise 2
+# Exercise 3
 class TaskCreation(TestCase):
 
-    def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.get("https://riot-todo-84334.firebaseapp.com/#!/")
-
-    def test_creation(self):
+    @Decorators.browsers()
+    def test_creation(self, driver, arguments):
+        browser = create_browser_driver(driver, arguments, "https://riot-todo-84334.firebaseapp.com/#!/")
         # Gets the number of current tasks
-        count = len(self.driver.find_elements_by_css_selector("li.animated.fadeIn"))
+        count = len(browser.find_elements_by_css_selector("li.animated.fadeIn"))
         # Create a new task
-        create_default_task(self.driver, "Lee", "Nothing to do here", "Seriously")
+        create_default_task(browser, "Lee", "Nothing to do here", "Seriously")
         # Confirms a new task was created
-        self.assertTrue(self.driver.find_element_by_css_selector("li.animated.fadeIn:nth-child({})".format(count + 1)))
+        self.assertTrue(browser.find_element_by_css_selector("li.animated.fadeIn:nth-child({})".format(count + 1)))
+        browser.quit()
 
-    def test_assignee_link(self):
+    @Decorators.browsers()
+    def test_assignee_link(self, driver, arguments):
+        browser = create_browser_driver(driver, arguments, "https://riot-todo-84334.firebaseapp.com/#!/")
         # Create a new task
-        create_default_task(self.driver, "Lee", "Nothing to do here", "Seriously")
+        create_default_task(browser, "Lee", "Nothing to do here", "Seriously")
         # Click assignee profile
-        assignee_elements = self.driver.find_elements_by_css_selector("span#assignee")
+        assignee_elements = browser.find_elements_by_css_selector("span#assignee")
         assignee_elements[-1].click()
         # Confirm I am on the expected profile page
-        self.assertEqual(self.driver.current_url, "https://riot-todo-84334.firebaseapp.com/#!/profile/Lee")
-
-    def tearDown(self):
-        self.driver.quit()
+        self.assertEqual(browser.current_url, "https://riot-todo-84334.firebaseapp.com/#!/profile/Lee")
+        browser.quit()
 
 
 def create_default_task(web_driver, assignee, title, content):
@@ -35,3 +34,9 @@ def create_default_task(web_driver, assignee, title, content):
     web_driver.find_element_by_css_selector("#taskTitle").send_keys(title)
     web_driver.find_element_by_css_selector("#taskContent").send_keys(content)
     web_driver.find_element_by_css_selector(".is-success.u-pull-right").click()
+
+
+def create_browser_driver(driver, arguments, link):
+    browser = driver(**arguments)  # if arguments else driver()
+    browser.get(link)
+    return browser
