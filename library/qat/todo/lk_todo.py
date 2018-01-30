4d305b508bc5c24dac32a0507c7b348911e17a82
Logan Kurr <lkurr@transparent,com>
2018-01-30 09:22:11 -0500

    Added lk_todo Created new create/delete methods

---
+++ library/qat/todo/lk_todo.py
@@ -0,0 +1,31 @@
+from pyscc import Controller
+from components.lk_home import Home
+from components.lk_profile import Profile
+from components.lk_header import Header
+
+
+class App(Controller):
+
+    def __init__(self, browser, base_url):
+        """Referencing the controller constructor"""
+        super(App, self).__init__(browser, base_url, {
+            'home': Home,
+            'header': Header,
+            'profile': Profile
+        })
+
+    def task_delete(self):
+        """Tests & Verifies task deletion"""
+        before = self.components.home.item.count()
+        self.components.home.task.delete.click()
+        self.wait(timeout=1)
+        assert before == (self.components.home.item.count() + 1)
+
+    def task_create(self, assignee, title, content):
+        """Tests & Verifies task creation"""
+        before = self.components.home.item.count()
+        self.components.home.create.assignee.send_input(assignee)
+        self.components.home.create.title.send_input(title)
+        self.components.home.create.content.send_input(content)
+        self.components.home.create.create.click()
+        assert before == (self.components.home.item.count() - 1)
